a082898db5b70c79ed747a79b2d57f747b7f090f
Githubshakil <shakilmahmud007@gmail.com>
2025-05-12 01:18:31 +0600

    route link set done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
+import ProductList from "./pages/ProductList";
+import ProductDetails from "./pages/ProductDetails";
+import CheckoutPage from "./pages/CheckoutPage";
+import BlogPage from "./pages/BlogPage";
+import ContactPage from "./pages/ContactPage";
 
 function App() {
   
@@ -8,6 +13,11 @@ function App() {
     <>
      <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/product-List" element={<ProductList />} />
+      <Route path="/product-details" element={<ProductDetails />} />
+      <Route path="/checkout-page" element={<CheckoutPage />} />
+      <Route path="/blog-page" element={<BlogPage />} />
+      <Route path="/contact-page" element={<ContactPage />} />
     </Routes>
     </>
   )
