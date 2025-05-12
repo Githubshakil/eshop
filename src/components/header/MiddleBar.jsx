659b961c2dd55bd8bffd4bdc2be1e0dd24729466
Githubshakil <shakilmahmud007@gmail.com>
2025-05-13 02:32:11 +0600

    update

--- src/components/header/MiddleBar.jsx
+++ src/components/header/MiddleBar.jsx
@@ -22,28 +22,32 @@ const MiddleBar = () => {
             <IoMdSearch className="text-3xl absolute top-1/2 right-6 -translate-y-1/2" />
           </div>
 
-          <div className=' flex flex-col justify-center relative pl-[50px] before:content-[""] before:bg-[#bfbfbf] before:absolute before:w-[1px] before:h-[32px] before:right-[-40px] before:top-[50%] before:-translate-[50%]'>
+          <div className='flex items-center  relative pl-[50px] before:content-[""] before:bg-[#bfbfbf] before:absolute before:w-[1px] before:h-[32px] before:right-[-40px] before:top-[50%] before:-translate-[50%]'>
             <div className="absolute top-[5px] left-0">
               <CartIcon />
             </div>
-            <p className='font-["Montserrat"] text-base font-normal leading-6'>
+            <div>
+              <p className='font-["Montserrat"] text-base font-normal leading-6'>
               Cart
             </p>
             <span className='font-["Montserrat"] font-bold text-sm leading-5'>
               $ 150,00
             </span>
+            </div>
           </div>
 
-          <div className="relative pl-[50px] ml-[89px] flex flex-col justify-center">
+          <div className="relative pl-[50px] ml-[89px] flex items-center">
             <div className="absolute top-[5px] left-0">
               <UserIcon />
             </div>
-            <p className='font-["Montserrat"] text-base font-normal leading-6'>
+           <div>
+             <p className='font-["Montserrat"] text-base font-normal leading-6'>
               User
             </p>
             <span className='font-["Montserrat"] font-bold text-sm leading-5'>
               Account
             </span>
+           </div>
           </div>
         </div>
       </div>
