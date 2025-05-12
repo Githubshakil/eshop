3aff5a049d3e20ba2f23eed719b8957bfd8daa84
Githubshakil <shakilmahmud007@gmail.com>
2025-05-13 01:06:48 +0600

    middle bar search bar done

--- src/components/header/MiddleBar.jsx
+++ src/components/header/MiddleBar.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import Container from '../commonlayout/Container'
+import { IoMdSearch } from "react-icons/io";
 
 const MiddleBar = () => {
   return (
     <Container>
-      <div className='flex justify-between items-center p-[32px]'>
+      
+      <div className='flex justify-between items-center py-8'>
         <div>
           <img src="images/logo.png" alt="logo" />
         </div>
-        <div>Others</div>
+
+
+        <div className='flex items-center'>
+          <div className='relative flex items-center mr-[48px]'>
+            <input 
+            className='w-[332px] border border-[rgba(48,48,48,0.35)] py-[18px] px-6 rounded-[10px]' 
+            type="text"
+            placeholder='Search Products ...' 
+            />
+            <IoMdSearch className='text-3xl absolute top-1/2 right-6 -translate-y-1/2' />
+          </div>
+          <div>cart</div>
+          <div>account</div>
+        </div>
       </div>
+
     </Container>
   )
 }
