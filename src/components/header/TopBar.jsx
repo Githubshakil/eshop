4459cecfd041be8b80a755cc15f83ef59fc4d3e8
Githubshakil <shakilmahmud007@gmail.com>
2025-05-12 19:16:16 +0600

    update

--- src/components/header/TopBar.jsx
+++ src/components/header/TopBar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Container from '../commonlayout/Container'
 
 const TopBar = () => {
   return (
-    <div className='border-b border-[red] border-solid '>TopBar</div>
+    <div className='border-b border-[rgba(48,48,48,0.25)]  border-solid   '>
+      <Container>
+        <div className='flex items-center justify-between '>
+          <div>Left</div>
+          <div>Right</div>
+        </div>
+      </Container>
+        
+      
+    </div>
   )
 }
 
