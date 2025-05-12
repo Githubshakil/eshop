e0f758e75ac76d465aff2e9037e149ed4435e037
Githubshakil <shakilmahmud007@gmail.com>
2025-05-13 00:52:20 +0600

    logo upload done

--- src/components/header/MiddleBar.jsx
+++ src/components/header/MiddleBar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Container from '../commonlayout/Container'
 
 const MiddleBar = () => {
   return (
-    <div>MiddleBar</div>
+    <Container>
+      <div className='flex justify-between items-center p-[32px]'>
+        <div>
+          <img src="images/logo.png" alt="logo" />
+        </div>
+        <div>Others</div>
+      </div>
+    </Container>
   )
 }
 
