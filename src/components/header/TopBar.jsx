e1ca9e298a9e98627ab6d82876ec275dedc8d15d
Githubshakil <shakilmahmud007@gmail.com>
2025-05-12 19:27:14 +0600

    update contact top

--- src/components/header/TopBar.jsx
+++ src/components/header/TopBar.jsx
@@ -6,7 +6,10 @@ const TopBar = () => {
     <div className='border-b border-[rgba(48,48,48,0.25)]  border-solid   '>
       <Container>
         <div className='flex items-center justify-between '>
-          <div>Left</div>
+          <div>
+            <a href="https://maps.app.goo.gl/gZ7SG15p6xTnDNrT8" target='_blank'>123 Main Street, Anytown USA</a>
+            <a href="tel: +1 (555) 123-4567">+1 (555) 123-4567</a>
+          </div>
           <div>Right</div>
         </div>
       </Container>
