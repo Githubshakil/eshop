ce4eb6f636c42a24af733969860fab82726a60db
Githubshakil <shakilmahmud007@gmail.com>
2025-05-13 16:01:00 +0600

    footer top done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({text, bg="#ff624c", fontName="Montserrat", fontWeight="bold",size="xl", color="white"}) => {
+const Button = ({text, bg="#ff624c", fontName="Montserrat", fontWeight="bold",size="xl", color="white", paddingY="4", paddingX="10", radius="10"}) => {
   return (
-    <button className={`bg-[${bg}] font-${fontName} font-${fontWeight} text-${size} text-${color}`}>{text}</button>
+    <button className={`bg-[${bg}] font-${fontName} font-${fontWeight} text-${size} text-${color} py-${paddingY} px-${paddingX} rounded-[${radius}px] cursor-pointer`}>{text}</button>
   )
 }
 
