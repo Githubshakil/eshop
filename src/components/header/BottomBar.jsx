cf38a16be3d5ec26aa3d6ff9138eccf900c95596
Githubshakil <shakilmahmud007@gmail.com>
2025-05-13 13:28:02 +0600

    update bottom part

--- src/components/header/BottomBar.jsx
+++ src/components/header/BottomBar.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Container from '../commonlayout/Container'
+import { Link } from 'react-router-dom'
+import { FaBars } from "react-icons/fa6";
+import { FaAngleDown } from "react-icons/fa6";
 
 const BottomBar = () => {
   return (
-    <div>BottomBar</div>
+    <div className='bg-[#ff624c]'>
+      <Container>
+        <div className='flex justify-between items-center py-6'>
+          <div>
+            <ul className='flex items-center gap-[80px] text-white font-["Montserrat"] font-bold text-'>
+              <li>
+                <Link to={"/product-list"} className='flex items-center gap-4'> <FaBars /> All Categories</Link>
+              </li >
+              <li>
+                <Link to={"/product-list"} className='flex items-center gap-4'>
+                Products
+                <FaAngleDown />
+                </Link>
+              </li>  
+              <li>
+                <Link to={"/blog-page"}>Blog</Link>
+              </li>  
+              <li>
+                <Link to={"/contact-page"}>Contact</Link>
+              </li>
+            </ul>
+          </div>
+                
+          <div>right</div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
