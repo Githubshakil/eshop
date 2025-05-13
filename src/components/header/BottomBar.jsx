c457a05e8a8364e4795e4b1883a0431c63ed2b8e
Githubshakil <shakilmahmud007@gmail.com>
2025-05-13 13:33:58 +0600

    update

--- src/components/header/BottomBar.jsx
+++ src/components/header/BottomBar.jsx
@@ -1,39 +1,61 @@
-import React from 'react'
-import Container from '../commonlayout/Container'
-import { Link } from 'react-router-dom'
+import React from "react";
+import Container from "../commonlayout/Container";
+import { Link } from "react-router-dom";
 import { FaBars } from "react-icons/fa6";
 import { FaAngleDown } from "react-icons/fa6";
 
 const BottomBar = () => {
   return (
-    <div className='bg-[#ff624c]'>
+    <div className="bg-[#ff624c]">
       <Container>
-        <div className='flex justify-between items-center py-6'>
+        <div className="flex justify-between items-center py-6">
           <div>
-            <ul className='flex items-center gap-[80px] text-white font-["Montserrat"] font-bold text-'>
+            <ul className='flex items-center gap-[80px] text-white font-["Montserrat"] font-bold text-base leading-6'>
               <li>
-                <Link to={"/product-list"} className='flex items-center gap-4'> <FaBars /> All Categories</Link>
-              </li >
-              <li>
-                <Link to={"/product-list"} className='flex items-center gap-4'>
-                Products
-                <FaAngleDown />
+                <Link to={"/product-list"} className="flex items-center gap-4">
+                  {" "}
+                  <FaBars /> All Categories
                 </Link>
-              </li>  
+              </li>
+              <li>
+                <Link to={"/product-list"} className="flex items-center gap-4">
+                  Products
+                  <FaAngleDown />
+                </Link>
+              </li>
               <li>
                 <Link to={"/blog-page"}>Blog</Link>
-              </li>  
+              </li>
               <li>
                 <Link to={"/contact-page"}>Contact</Link>
               </li>
             </ul>
           </div>
-                
-          <div>right</div>
+
+          <div>
+            <div>
+              <ul className='flex items-center gap-[80px] text-white font-["Montserrat"] font-bold text-base leading-6'>
+                <li>
+                  <Link to={"#"} className="flex items-center gap-4">
+                    {" "}
+                    LIMITED SALE 👋🏻
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"#"} className="flex items-center gap-4">
+                    Best Seller
+                  </Link>
+                </li>
+                <li>
+                  <Link to={"#"}>New Arrival</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default BottomBar
+export default BottomBar;
