2ef9befe3b243387d02188c1e06d37d6ab9928c5
Githubshakil <shakilmahmud007@gmail.com>
2025-05-13 20:08:23 +0600

    update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({text, bg="#ff624c", fontName="Montserrat", fontWeight="bold",size="xl", color="white", paddingY="4", paddingX="10", radius="10"}) => {
   return (
-    <button className={`bg-[${bg}] font-${fontName} font-${fontWeight} text-${size} text-${color} py-${paddingY}px px-${paddingX}px rounded-[${radius}px] cursor-pointer`}>{text}</button>
+    <button className={`bg-[${bg}] font-${fontName} font-${fontWeight} text-${size} text-${color} py-${paddingY} px-${paddingX} rounded-[${radius}px] cursor-pointer`}>{text}</button>
   )
 }
 
