8b211a9c787781a2106901dd79aa122c3c4fbad1
Githubshakil <shakilmahmud007@gmail.com>
2025-05-15 00:10:00 +0600

    contact footer bottom update

--- src/components/footer/FooterBottom.jsx
+++ src/components/footer/FooterBottom.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import  Container  from '../commonlayout/Container';
 import { IoLocationOutline } from 'react-icons/io5'
 import { BsTelephone } from 'react-icons/bs'
+import { FaEnvelope } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 const FooterBottom = () => {
@@ -11,28 +12,37 @@ const FooterBottom = () => {
         <div className='flex'>
           <div >
               <img src="images/logo.png" alt="logo" />
-              <ul >
-                <li className='relative left-8 mt-[158px]'>
-                  <Link to="tel: +1 (555) 123-4567" >
-                    <BsTelephone className=' absolute top-[20%] -left-8 ' />
+              <ul className='relative ' >
+                <li className='  mt-[158px]'>
+                  <BsTelephone  />
+                  <div className=' absolute left-[24px] bottom-[54px] flex font-["Montserrat"] text-base text-[#303030] font-normal leading-6 '>
+                    <Link to="tel: +1 (555) 123-4567"  >
                     +1 (555) 123-4567
                   </Link>
+                  </div>
                 </li>
-                <li>
-                    <Link to="mailto: information@eshop.com " >
-                    <BsTelephone className=' absolute top-[20%] -left-8 ' />
-                    +1 (555) 123-4567
-                    </Link>
-                </li>
-                <li className='relative left-8 mt-3'>
-                  <Link to="https://maps.app.goo.gl/gZ7SG15p6xTnDNrT8" target='_blank' className='flex items-center gap-2'>
-                     <IoLocationOutline className=' absolute top-[20%] -left-8 '/>
-                    123 Main Street, Anytown USA
+
+                <li className='  mt-3'>
+                  <FaEnvelope />
+                  <div className=' absolute left-[26px]  top-[22px] flex font-["Montserrat"] text-base text-[#303030] font-normal leading-6  '>
+                    <Link to="mailto: information@eshop.com"  >
+                    information@eshop.com
                   </Link>
+                  </div>
+                </li>
+                <li className='  mt-3'>
+                  <IoLocationOutline  />
+                  <div className=' absolute left-6 top-[50px] -mr-[220px] font-["Montserrat"] text-base text-[#303030] font-normal leading-6 '>
+                    <Link to="https://maps.app.goo.gl/gZ7SG15p6xTnDNrT8" target='_blank' >
+                    123 Main Street, Suite 105, Anytown USA
+                  </Link>
+                  </div>
                 </li>
               </ul>
-            
           </div>
+                    
+                
+            
           <div>two</div>
           <div>Three</div>
           <div>four</div>
--- src/components/header/topbarComponents/TopLeftBar.jsx
+++ src/components/header/topbarComponents/TopLeftBar.jsx
@@ -7,7 +7,7 @@ const TopLeftBar = () => {
   return (
     <>
         <div className='flex items-center gap-[50px]  relative after:content-[""] after:bg-[#bfbfbf] after:absolute after:w-[1px] after:h-[32px] after:left-[62%] after:top-[50%] after:-translate-[50%]'>
-            <Link to="https://maps.app.goo.gl/gZ7SG15p6xTnDNrT8" target='_blank' className='flex items-center gap-2'>
+            <Link to="https://maps.app.goo.gl/gZ7SG15p6xTnDNrT8" target='_blank' className='flex items-center gap-2 '>
               <IoLocationOutline />
               123 Main Street, Anytown USA
             </Link>
