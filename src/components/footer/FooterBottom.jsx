b313f8e5756c187d85a4345fc2c01c570fa88f89
Githubshakil <shakilmahmud007@gmail.com>
2025-05-14 22:37:49 +0600

    update footer bottom

--- src/components/footer/FooterBottom.jsx
+++ src/components/footer/FooterBottom.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import  Container  from '../commonlayout/Container';
 
 const FooterBottom = () => {
   return (
-    <div>FooterBottom</div>
+    <div className='mt-[100px]'>
+      <Container>
+        <div className='flex'>
+          <div>one</div>
+          <div>two</div>
+          <div>Three</div>
+          <div>four</div>
+          <div>five</div>
+        </div>
+      </Container>
+    </div>
   )
 }
 
