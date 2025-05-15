d677bceba3c7fba6d541c3bd863d4d139a0b8383
Githubshakil <shakilmahmud007@gmail.com>
2025-05-15 15:54:31 +0600

    banner section done

---
+++ src/components/Banner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Container from './commonlayout/Container'
+import { Link } from 'react-router-dom'
+
+const Banner = () => {
+  return (
+    <>
+        <Container>
+            <div className='mt-8 mb-20'>
+                <Link to='/'>
+                    <img src="images/banner.png" alt="banner" />
+                </Link>
+            </div>
+        </Container>
+    </>
+  )
+}
+
+export default Banner
