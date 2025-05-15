a416e3c3a01848bc8ce2e538dbcf5375a6f934e9
Githubshakil <shakilmahmud007@gmail.com>
2025-05-15 16:30:20 +0600

    FacilitySection done

---
+++ src/components/FacilitySection.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import Container from './commonlayout/Container'
+import Headphone from '../icons/Headphone'
+import Security from '../icons/Security'
+import Delivery from '../icons/Delivery'
+import Transparent from '../icons/Transparent'
+
+
+
+const FacilitySection = () => {
+  return (
+    <Container>
+        <div className='flex justify-between items-center mb-20'>
+            
+                <div className='flex items-center gap-6'>
+                    <div>
+                        <Headphone/>
+                    </div>
+                    <div>
+                        <h6 className='font-["Montserrat"] text-base text-normal font-bold leading-6 text-[#303030]'>Responsive</h6>
+                        <p className='font-["Montserrat"] text-base text-normal font-normal leading-6  text-[#303030]'>Customer service available 24/7</p>
+                    </div>
+                    
+                </div>
+
+                <div className='flex items-center gap-6'>
+                    <div>
+                        <Security/>
+                    </div>
+                    <div>
+                        <h6 className='font-["Montserrat"] text-base text-normal font-bold leading-6 text-[#303030]'>Secure</h6>
+                        <p className='font-["Montserrat"] text-base text-normal font-normal leading-6  text-[#303030]'>Certified marketplace since 2017</p>
+                    </div>
+                    
+                </div>
+
+                <div className='flex items-center gap-6'>
+                    <div>
+                        <Delivery/>
+                    </div>
+                    <div>
+                        <h6 className='font-["Montserrat"] text-base text-normal font-bold leading-6 text-[#303030]'>Shipping</h6>
+                        <p className='font-["Montserrat"] text-base text-normal font-normal leading-6  text-[#303030]'>Free, fast, and reliable worldwide</p>
+                    </div>
+                    
+                </div>
+
+                <div className='flex items-center gap-6'>
+                    <div>
+                        <Transparent />
+                    </div>
+                    <div>
+                        <h6 className='font-["Montserrat"] text-base text-normal font-bold leading-6 text-[#303030]'>Transparent</h6>
+                        <p className='font-["Montserrat"] text-base text-normal font-normal leading-6  text-[#303030]'>Hassle-free return policy</p>
+                    </div>
+                    
+                </div>
+        </div>
+                
+                
+                
+            
+
+        
+    </Container>
+  )
+}
+
+export default FacilitySection
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import FacilitySection from '../components/FacilitySection'
 
 const HomePage = () => {
   return (
     <>
     <Banner />
+    <FacilitySection />
     </>
   )
 }
