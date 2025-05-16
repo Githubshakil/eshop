b6527a0fcd23ca2380989e1d91885d71921e07e1
Githubshakil <shakilmahmud007@gmail.com>
2025-05-16 16:05:36 +0600

    update product layout

--- src/components/commonlayout/ProductLayout.jsx
+++ src/components/commonlayout/ProductLayout.jsx
@@ -3,20 +3,20 @@ import { IoStar } from "react-icons/io5";
 
 const ProductLayout = () => {
   return (
-    <div className=' border border-gray-500 p-6 bg-gray-700'>
+    <div className=' border border-gray-500 p-6  rounded-2xl'>
         <div>
             <img src="images/productimg.png" alt="productimg" />
         </div>
         <div>
-            <p>LAPTOP</p>
-            <h3>S21 Laptop Ultra HD LED Screen Feature 2023 ...</h3>
-            <div className='flex items-center gap-2 text-yellow-500'>
+            <p className='font-["Montserrat"] font-normal text-sm leading-5 uppercase  tracking-[5px] mt-[46px] mb-4 text-[#303030]'>LAPTOP</p>
+            <h3 className='font-["Poppins"] font-semibold text-5 leading-[30px]'>S21 Laptop Ultra HD LED Screen Feature 2023 ...</h3>
+            <div className='flex items-center gap-2 text-[#fed550]'>
                 <IoStar />
                 <IoStar />
                 <IoStar />
                 <IoStar />
                 <IoStar />
-                <span>( 100 )</span>
+                <span className='text-[#303030]'>( 100 )</span>
             </div>
             <p>$1,199.00</p>
 
