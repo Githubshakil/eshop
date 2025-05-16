ad026be7e3c20388b47bc28abbee54d6b6e649eb
Githubshakil <shakilmahmud007@gmail.com>
2025-05-16 14:49:13 +0600

    update product layout

---
+++ src/components/FeatureProduct.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ProductLayout from './commonlayout/ProductLayout'
+import  Container  from '../components/commonlayout/Container'
+
+const FeatureProduct = () => {
+  return (
+    <Container>
+            
+        <div>
+            <ProductLayout /> 
+            <ProductLayout /> 
+            <ProductLayout /> 
+            <ProductLayout /> 
+            <ProductLayout /> 
+        </div>
+    </Container>
+  )
+}
+
+export default FeatureProduct
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import FacilitySection from '../components/FacilitySection'
+import FeatureProduct from '../components/FeatureProduct'
 
 const HomePage = () => {
   return (
     <>
     <Banner />
     <FacilitySection />
+    <FeatureProduct />
     </>
   )
 }
