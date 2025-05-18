bc0c0ad0fcda4a7181202ce5cfa5c6958285c079
Githubshakil <shakilmahmud007@gmail.com>
2025-05-18 14:54:05 +0600

    update

--- src/components/header/topbarComponents/TopRightBar.jsx
+++ src/components/header/topbarComponents/TopRightBar.jsx
@@ -6,47 +6,44 @@ import { FaInstagram } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 const TopRightBar = () => {
-
     const [selectedCountry, setSelectedCountry] = useState(null);
     const [isOpen, setIsOpen] = useState(false);
     const [selectedCurrency, setSelectedCurrency] = useState("USD");
-
-
+    
+    
     const countryDropdownRef = useRef(null)
-
+    
     useEffect(()=>{
         const handleClickOutSide = (event) => {
             if(countryDropdownRef.current && !countryDropdownRef.current.contains(event.target)){
                 setIsOpen(false)
             }
         }
-
+        
         document.addEventListener('mousedown', handleClickOutSide)
     },[])
-
+    
     const countries = [
-        { name: "United State", value: "us" , flag: "https://flagcdn.com/16x12/us.png" },
-        { name: "Spain", value: "es" , flag: "https://flagcdn.com/16x12/es.png" },
-        { name: "France", value: "fr" , flag: "https://flagcdn.com/16x12/fr.png" },
-        { name: "Germany", value: "de" , flag: "https://flagcdn.com/16x12/de.png" },
-        { name: "Italy", value: "it" , flag: "https://flagcdn.com/16x12/it.png" },
-        { name: "Portugal", value: "pt" , flag: "https://flagcdn.com/16x12/pt.png" },
-        { name: "Netherlands", value: "nl" , flag: "https://flagcdn.com/16x12/nl.png" },
-        { name: "Sweden", value: "se" , flag: "https://flagcdn.com/16x12/se.png" },
-        { name: "Norway", value: "no" , flag: "https://flagcdn.com/16x12/no.png" },
-        { name: "Denmark", value: "dk" , flag: "https://flagcdn.com/16x12/dk.png" },
-        { name: "Bangladesh", value: "bd" , flag: "https://flagcdn.com/16x12/bd.png" },
-        { name: "India", value: "in" , flag: "https://flagcdn.com/16x12/in.png" },
-        { name: "Pakistan", value: "pk" , flag: "https://flagcdn.com/16x12/pk.png" },
-        { name: "Nepal", value: "np" , flag: "https://flagcdn.com/16x12/np.png" },
-        { name: "Bhutan", value: "bt" , flag: "https://flagcdn.com/16x12/bt.png" },
-        { name: "Sri Lanka", value: "lk" , flag: "https://flagcdn.com/16x12/lk.png" },
-        { name: "Maldives", value: "mv" , flag: "https://flagcdn.com/16x12/mv.png" },
-            
-       
+        { name: "United State", value: "us", language: "en", flag: "https://flagcdn.com/16x12/us.png" },
+        { name: "Spain", value: "es", language: "es", flag: "https://flagcdn.com/16x12/es.png" },
+        { name: "France", value: "fr", language: "fr", flag: "https://flagcdn.com/16x12/fr.png" },
+        { name: "Germany", value: "de", language: "de", flag: "https://flagcdn.com/16x12/de.png" },
+        { name: "Italy", value: "it", language: "it", flag: "https://flagcdn.com/16x12/it.png" },
+        { name: "Portugal", value: "pt", language: "pt", flag: "https://flagcdn.com/16x12/pt.png" },
+        { name: "Netherlands", value: "nl", language: "nl", flag: "https://flagcdn.com/16x12/nl.png" },
+        { name: "Sweden", value: "se", language: "sv", flag: "https://flagcdn.com/16x12/se.png" },
+        { name: "Norway", value: "no", language: "no", flag: "https://flagcdn.com/16x12/no.png" },
+        { name: "Denmark", value: "dk", language: "da", flag: "https://flagcdn.com/16x12/dk.png" },
+        { name: "Bangladesh", value: "bd", language: "bn", flag: "https://flagcdn.com/16x12/bd.png" },
+        { name: "India", value: "in", language: "hi", flag: "https://flagcdn.com/16x12/in.png" },
+        { name: "Pakistan", value: "pk", language: "ur", flag: "https://flagcdn.com/16x12/pk.png" },
+        { name: "Nepal", value: "np", language: "ne", flag: "https://flagcdn.com/16x12/np.png" },
+        { name: "Bhutan", value: "bt", language: "dz", flag: "https://flagcdn.com/16x12/bt.png" },
+        { name: "Sri Lanka", value: "lk", language: "si", flag: "https://flagcdn.com/16x12/lk.png" },
+        { name: "Maldives", value: "mv", language: "dv", flag: "https://flagcdn.com/16x12/mv.png" },
     ];
-
-
+    
+    
     const currency = [
         { name: "USD", value: "USD" },
         { name: "BDT", value: "BDT" },
@@ -58,16 +55,16 @@ const TopRightBar = () => {
         { name: "CAD", value: "CAD" },
         
     ]
-
+    
     const handleSelect = (country) => {
         setSelectedCountry(country);
         setIsOpen(false);
     }
-   
-
-
-  return (
-    <div className='flex justify-end items-center gap-[49px] '>
+    
+    
+    
+    return (
+        <div className='flex justify-end items-center gap-[49px] '>
         <div>
             <select 
             className='cursor-pointer'
@@ -76,6 +73,7 @@ const TopRightBar = () => {
             onChange={(e)=>{
                 const currency = e.target.value;
                 setSelectedCurrency(currency);
+                
                 
             }
         }
@@ -100,14 +98,18 @@ const TopRightBar = () => {
         >
             <select className='w-[150px] hidden' name="country"
             value={selectedCountry?.value || ""}
+            
             onChange={(e)=>{
               const country = countries.find((c) => c.value === e.target.value)
+              
               setSelectedCountry(country);
             }}
             >
                {countries.map((country, index)=>(
                    <option key={index}
-                   value={country.name}>{country.name}</option>
+                   value={country.name}
+                   language={country.language}
+                   >{country.name}</option>
 
                ))}
             </select>
@@ -140,6 +142,7 @@ const TopRightBar = () => {
                         {countries.map((country, index)=>(
                             <li
                                 key={country.value}
+                                
                              className='flex items-center gap-2 p-2 hover:bg-gray-200 cursor-pointer'
                              onClick={()=> handleSelect(country)}
                              >
