c74d6dc5f855ae9ef0a15a6b0a83fd069a83fc58
Githubshakil <shakilmahmud007@gmail.com>
2025-05-24 03:14:03 +0600

    update

--- src/components/homePage/TechTalkSection.jsx
+++ src/components/homePage/TechTalkSection.jsx
@@ -15,33 +15,33 @@ const TechTalkSection = () => {
             <SideButton sidetext={"View All"}/>
         </div>
         <div className='flex items-center gap-12 '>
-            <div className=' border border-transparent max-w-[544px] px-[52px] py-[52px] hover:border-[rgba(48,48,48,0.25)]  hover:border- duration-300  rounded-[25px] cursor-pointer'>
+            <div className=' border border-transparent  hover:border-[rgba(48,48,48,0.25)]  hover:border- duration-300  rounded-[25px] cursor-pointer'>
                 <div className='w-[440px] h-[406px] rounded-[25px] bg-[#D9D9D9] mb-[32px] mx-auto duration-300'></div>
                 <h5 className='font-["Poppins"] text-2xl font-semibold leading-[30px] mb-8 w-[439px]'>Our Category Choices for Top 10 Tech Gadgets in 2023</h5>
-                <div className='flex items-center justify-between  '>
-                    <button className='py--[7px] px-5 bg-[#ff624c] text-white rounded-[10px] text-center  '>Gadgets</button>
+                <div className='flex items-center justify-between mb-12 '>
+                    <button className='py-[7px] px-5 bg-[#ff624c] text-white rounded-[10px] text-center  '>Gadgets</button>
                     <div className='flex items-center gap-[10px]'>
                         <Clock/><span>10 April 2023</span>
                     </div>
                 </div>
             </div>
 
-            <div className=' border border-transparent max-w-[544px] px-[52px] py-[52px] hover:border-[rgba(48,48,48,0.25)]  hover:border- duration-300  rounded-[25px] cursor-pointer'>
+            <div className=' border border-transparent   hover:border-[rgba(48,48,48,0.25)]  hover:border- duration-300  rounded-[25px] cursor-pointer'>
                 <div className='w-[440px] h-[406px] rounded-[25px] bg-[#D9D9D9] mb-[32px] mx-auto duration-300'></div>
                 <h5 className='font-["Poppins"] text-2xl font-semibold leading-[30px] mb-8 w-[439px]'>Our Category Choices for Top 10 Tech Gadgets in 2023</h5>
                 <div className='flex items-center justify-between mb-12 '>
-                    <button className='py--[7px] px-5 bg-[#ff624c] text-white rounded-[10px] text-center  '>Tips</button>
+                    <button className='py-[7px] px-5 bg-[#ff624c] text-white rounded-[10px] text-center  '>Tips</button>
                     <div className='flex items-center gap-[10px]'>
                         <Clock/><span>12 April 2023</span>
                     </div>
                 </div>
             </div>
 
-            <div className=' border border-transparent max-w-[544px] px-[52px] py-[52px] hover:border-[rgba(48,48,48,0.25)]  hover:border- duration-300  rounded-[25px] cursor-pointer'>
+            <div className=' border border-transparent   hover:border-[rgba(48,48,48,0.25)]  hover:border- duration-300  rounded-[25px] cursor-pointer'>
                 <div className='w-[440px] h-[406px] rounded-[25px] bg-[#D9D9D9] mb-[32px] mx-auto duration-300'></div>
                 <h5 className='font-["Poppins"] text-2xl font-semibold leading-[30px] mb-8 w-[439px]'>Our Category Choices for Top 10 Tech Gadgets in 2023</h5>
                 <div className='flex items-center justify-between mb-12 '>
-                    <button className='py--[7px] px-5 bg-[#ff624c] text-white rounded-[10px] text-center  '>Review</button>
+                    <button className='py-[7px] px-5 bg-[#ff624c] text-white rounded-[10px] text-center  '>Review</button>
                     <div className='flex items-center gap-[10px]'>
                         <Clock/><span>16 April 2023</span>
                     </div>
