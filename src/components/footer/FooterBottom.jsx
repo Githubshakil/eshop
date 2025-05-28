185f59844831fba0d486c84462f7e7325050b434
Githubshakil <shakilmahmud007@gmail.com>
2025-05-29 01:37:16 +0600

    update

--- src/components/footer/FooterBottom.jsx
+++ src/components/footer/FooterBottom.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Container from "../commonlayout/Container";
-import { IoLocationOutline } from "react-icons/io5";
-import { BsTelephone } from "react-icons/bs";
-import { FaEnvelope } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import VisaIcon from "../../icons/VisaIcon";
 import MasterCardIcon from "../../icons/MasterCardIcon";
 import PayIcon from "../../icons/PayIcon";
 import PaypalIcon from "../../icons/PaypalIcon";
+import PhoneFooter from "../../icons/PhoneFooter";
+import EmailFooter from "../../icons/EmailFooter";
+import LocationFooter from "../../icons/LocationFooter";
 
 const FooterBottom = () => {
   return (
@@ -17,36 +17,45 @@ const FooterBottom = () => {
           <div className="mb-[80px]">
             <img src="images/logo.png" alt="logo" />
             <ul className="relative ">
-              <li className="  mt-[158px]">
-                <BsTelephone />
-                <div className=' absolute left-[24px] bottom-[54px] -mr-[200px] flex font-["Montserrat"] text-base text-[#303030] font-normal leading-6 '>
-                  <Link to="tel: +1 (555) 123-4567">+1 (555) 123-4567</Link>
+              <li className="  mt-[158px] flex items-center gap-3 group">
+                <div className="flex items-center gap-3 group-hover:scale-120 transition-all duration-300 cursor-pointer group-hover:text-[#ff624c]">
+                  <PhoneFooter />
                 </div>
+                <Link
+                  to="tel: +1 (555) 123-4567"
+                  className="text-[#303030] font-['Montserrat'] text-base font-normal leading-6 group-hover:text-[#ff624c] transition-all duration-300"
+                >
+                  +1 (555) 123-4567
+                </Link>
               </li>
 
-              <li className="  mt-3">
-                <FaEnvelope />
-                <div className=' absolute left-[26px]  top-[22px] flex font-["Montserrat"] text-base text-[#303030] font-normal leading-6  '>
-                  <Link to="mailto: information@eshop.com">
-                    information@eshop.com
-                  </Link>
+              <li className="  mt-4 flex items-center gap-3 group">
+                <div className="flex items-center gap-3 group-hover:scale-120 transition-all duration-300 cursor-pointer group-hover:text-[#ff624c]">
+                  <EmailFooter />
                 </div>
+                <Link
+                  to="mailto: information@eshop.com"
+                  className="text-[#303030] font-['Montserrat'] text-base font-normal leading-6 group-hover:text-[#ff624c] transition-all duration-300"
+                >
+                  information@eshop.com
+                </Link>
               </li>
-              <li className="  mt-3">
-                <IoLocationOutline />
-                <div className=' absolute left-6 top-[50px] -mr-[220px] font-["Montserrat"] text-base text-[#303030] font-normal leading-6 '>
-                  <Link
-                    to="https://maps.app.goo.gl/gZ7SG15p6xTnDNrT8"
-                    target="_blank"
-                  >
-                    123 Main Street, Suite 105, Anytown USA
-                  </Link>
+              <li className="  mt-4 flex items-center gap-3 group">
+                <div className="flex items-center gap-3 group-hover:scale-120 transition-all duration-300 cursor-pointer group-hover:text-[#ff624c]">
+                  <LocationFooter />
                 </div>
+                <Link
+                  to="https://maps.app.goo.gl/gZ7SG15p6xTnDNrT8"
+                  target="_blank"
+                  className="  text-[#303030] font-['Montserrat'] text-base font-normal leading-6 hover:text-[#ff624c] transition-all duration-300"
+                >
+                  123 Main Street, Suite 105, Anytown USA
+                </Link>
               </li>
             </ul>
           </div>
 
-          <div className="ml-[387px]">
+          <div className="ml-[167px]">
             <h4 className='font-["Poppins"] font-semibold text-xl text-[#303030]'>
               Links
             </h4>
@@ -155,21 +164,21 @@ const FooterBottom = () => {
         </div>
         <div className=""></div>
 
-        <div className="flex justify-between">
+        <div className="flex justify-between mt-2">
           <div>
-            <span className='font-["Montserrat"] text-[14px] text-[#303030] leading-5 font-normal'>
+            <span className='font-["Montserrat"] text-sm text-[rgba(48,48,48,0.75)] leading-5 font-normal'>
               Copyright © 2023 E-Shop. All Rights Reserved.
             </span>
           </div>
 
           <div className="flex gap-5 items-center">
-            <h5 className='font-["Montserrat"] font-normal text-[14px] leading-5 text-[#303030] hover:text-[#ff624c] duration-300 '>
+            <h5 className='font-["Montserrat"] font-normal text-[14px] leading-5 text-[rgba(48,48,48,0.75)] hover:text-[#ff624c] duration-300 '>
               <Link to={"#"}>Privacy Policy</Link>
             </h5>
-            <h5 className='font-["Montserrat"] font-normal text-[14px] leading-5 text-[#303030] relative after:content-[""] after:bg-[#bfbfbf] after:absolute after:w-[1px] after:h-[16px] after:left-[-10px] after:top-[55%] after:-translate-[50%] before:content-[""] before:bg-[#bfbfbf] before:absolute before:w-[1px] before:h-[16px] before:right-[-10px] before:top-[55%] before:-translate-[50%] hover:text-[#ff624c] duration-300'>
+            <h5 className='font-["Montserrat"] font-normal text-[14px] leading-5 text-[rgba(48,48,48,0.75)] relative after:content-[""] after:bg-[#bfbfbf] after:absolute after:w-[1px] after:h-[16px] after:left-[-10px] after:top-[55%] after:-translate-[50%] before:content-[""] before:bg-[#bfbfbf] before:absolute before:w-[1px] before:h-[16px] before:right-[-10px] before:top-[55%] before:-translate-[50%] hover:text-[#ff624c] duration-300'>
               <Link to={"#"}>Terms & Condition</Link>
             </h5>
-            <h5 className='font-["Montserrat"] font-normal text-[14px] leading-5 text-[#303030] hover:text-[#ff624c] duration-300'>
+            <h5 className='font-["Montserrat"] font-normal text-[14px] leading-5 text-[rgba(48,48,48,0.75)] hover:text-[#ff624c] duration-300'>
               <Link to={"#"}>Sitemap</Link>
             </h5>
           </div>
