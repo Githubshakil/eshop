da601b25640e8e71225f4d4532dcee32ec4911ac
Githubshakil <shakilmahmud007@gmail.com>
2025-06-15 00:28:25 +0600

    update

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -3,10 +3,10 @@ import Container from '../../src/components/commonlayout/Container'
 
 
 const ProductList = () => {
-  let [minValue, setMinValue] = useState(minValue)
-  let [maxValue, setMaxValue] = useState(maxValue)
+  let [minValue, setMinValue] = useState(0)
+  let [maxValue, setMaxValue] = useState(1000)
 
-  let []
+  
 
 
   const updateSlider = (type, value)=>{
@@ -19,8 +19,8 @@ const ProductList = () => {
     }
   }
 
-   const minPercent =(minValue / maxValue) * 100
-   const maxPercent =(maxValue / minValue) * 100
+   const minPercent =(minValue / 1000) * 100
+   const maxPercent =(maxValue / 1000) * 100
 
 
 
@@ -31,9 +31,9 @@ const ProductList = () => {
         <div className='w-[355px] bg-[#f4f4f4] rounded-[25px] p-[48px]'>
           <h3 className='font-["Montserrat"] font-bold text-[20px] '>Price</h3>
           <div className='mt-6' >
-            <div className='flex justify-between mb-[30px]'>
-              <span className='w-[124px] py-[25px] px-[42px] border border-[rgba(48,48,48,0.25)] rounded-[10px] bg-[#f4f4f4]'></span>
-              <span className='py-[25px] px-[42px] border border-[rgba(48,48,48,0.25)] rounded-[10px] bg-[#f4f4f4]'></span>
+            <div className='flex justify-between mb-[30px] gap-3'>
+              <span className='w-[124px] py-[25px] px-[42px] border border-[rgba(48,48,48,0.25)] rounded-[10px] bg-[#f4f4f4]'>${minValue}</span>
+              <span className='w-[124px] py-[25px] px-[42px] border border-[rgba(48,48,48,0.25)] rounded-[10px] bg-[#f4f4f4]'>${maxValue}</span>
             </div>
               <div className='relative w-full h-[2px] bg-[#e8e8e8] rounded '>
                 <div 
