30db1ea7a693dddf2c62c220676710cd06464f99
Githubshakil <shakilmahmud007@gmail.com>
2025-06-14 20:50:36 +0600

    product page update

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Container from '../../src/components/commonlayout/Container'
+
 
 const ProductList = () => {
   return (
-    <div>ProductList</div>
+    <>
+    <Container>
+      <div className='flex'>
+        <div className='w-[355px] bg-[#f4f4f4] rounded-[25px] p-[48px]'>
+          <h3 className='font-["Montserrat"] font-bold text-[20px] '>Price</h3>
+        </div>
+        <div className='w-[80%]'>asd</div>
+      </div>
+    </Container>
+    </>
   )
 }
 
